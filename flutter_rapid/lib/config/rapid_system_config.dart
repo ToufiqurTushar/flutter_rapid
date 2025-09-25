65b83ea389ba6a6b380668b50d320a809cba3e63
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2025-09-25 09:34:53 +0600

    Remove Chucker

--- flutter_rapid/lib/config/rapid_system_config.dart
+++ flutter_rapid/lib/config/rapid_system_config.dart
@@ -32,7 +32,7 @@ class RapidSystemConfig {
   String currentEnv = "dev";
   Map<String, RapidEnvConfig> availableEnvironment = {};
 
-  List<NavigatorObserver> navigatorObservers;
+  List<NavigatorObserver> navigatorObservers = [];
 
   initConfig(var env) {
     return Get.put<RapidEnvConfig>(env, permanent: true);
