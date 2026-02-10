beada4796598b3e7826c931d39902b9d0a4e5bfc
ToufiqurTushar <75735441+ToufiqurTushar@users.noreply.github.com>
2026-02-10 11:08:21 +0600

    Library Update

--- flutter_rapid/lib/base/flutter_rapid_app.dart
+++ flutter_rapid/lib/base/flutter_rapid_app.dart
@@ -72,6 +72,7 @@ class FlutterRapidApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     RLog.error(Get.locale.toString());
+    RLog.error('locale:${Get.locale?.toString()??'en_US'}');
     globalStateLogic.availableLocal = _sysConf.availableLocal;
     globalStateLogic.availableFont = _sysConf.availableFont;
 
